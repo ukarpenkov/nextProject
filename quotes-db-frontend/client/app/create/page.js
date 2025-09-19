5148651ecd2c37638ab99616874bfcc39183d8a5
ukarpenkov <truloveu@gmail.com>
2025-09-19 16:22:49 +0300

    Add Empty search page and route to this

--- quotes-db-frontend/client/app/create/page.js
+++ quotes-db-frontend/client/app/create/page.js
@@ -2,7 +2,6 @@ function CreateNewQuotePage() {
     return (
         <div>
             <h1>Create New Quote Page</h1>
-            {/* Add your form or content for creating a new quote here */}
         </div>
     )
 }
