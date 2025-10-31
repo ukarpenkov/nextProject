9be07fc58442861254627191b17dd3f5a85e44c4
ukarpenkov <truloveu@gmail.com>
2025-10-31 15:37:25 +0300

    ref

--- quotes-db-frontend/server/src/middlewares/categoryValidators.js
+++ quotes-db-frontend/server/src/middlewares/categoryValidators.js
@@ -1,25 +1,23 @@
-const { query, param } = require('express-validator');
+const { query, param } = require("express-validator");
 
-// Allows lowercase letters, numbers and dashes
 const CATEGORY_NAME_REGEX = /^[a-z0-9\-]+$/;
-// Allows any letters, numbers and dashes
 const CATEGORY_NAME_I_REGEX = /^[a-z0-9\-]+$/i;
 
 const getCategoriesValidators = [
-  query('limit').optional().trim().isInt({ min: 1, max: 50 }),
-  query('offset').optional().trim().isInt({ min: 0 }),
-  query('name')
+  query("limit").optional().trim().isInt({ min: 1, max: 50 }),
+  query("offset").optional().trim().isInt({ min: 0 }),
+  query("name")
     .optional()
     .trim()
     .escape()
     .custom((value) =>
       CATEGORY_NAME_I_REGEX.test(value)
         ? Promise.resolve()
-        : Promise.reject('Category can only contain letters and dashes')
+        : Promise.reject("Category can only contain letters and dashes")
     ),
 ];
 
-const getSingleCategoryValidators = [param('id').trim().isInt({ min: 1 })];
+const getSingleCategoryValidators = [param("id").trim().isInt({ min: 1 })];
 
 module.exports = {
   CATEGORY_NAME_REGEX,
