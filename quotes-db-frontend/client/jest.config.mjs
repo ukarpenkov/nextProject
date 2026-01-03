e95eba9bd88a2180316b05b5a0a564aad1d8f9b8
ukarpenkov <truloveu@gmail.com>
2026-01-03 12:19:43 +0300

    update Jest configuration

--- quotes-db-frontend/client/jest.config.mjs
+++ quotes-db-frontend/client/jest.config.mjs
@@ -1,11 +1,12 @@
-export default {
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+    dir: './',
+})
+
+const customJestConfig = {
     testEnvironment: 'jsdom',
-    transform: {
-        '^.+\\.(js|jsx)$': 'babel-jest',
-    },
-    moduleFileExtensions: ['js', 'jsx'],
     moduleNameMapper: {
-        // Mock CSS modules and asset imports
         '^.+\\.(css|less|scss|sass)$': 'identity-obj-proxy',
         '^@/(.*)$': '<rootDir>/$1',
     },
@@ -13,3 +14,5 @@ export default {
     testMatch: ['**/__tests__/**/*.(js|jsx)', '**/?(*.)+(spec|test).(js|jsx)'],
     collectCoverageFrom: ['app/**/*.{js,jsx}', 'components/**/*.{js,jsx}'],
 }
+
+export default await createJestConfig(customJestConfig)
